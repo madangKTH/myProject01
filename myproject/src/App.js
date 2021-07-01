29ae4c914925f58d6c85c52510c663a6ab7e3350
madangKTH <th503@naver.com>
2021-07-02 00:02:20 +0900

    test02

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import  ImportComponent from './R003_ImportComponent'
+import  LifecycleEx from './R005_LifecycleEx'
 function App() {
   return (
     <div>
       <h1>Start React 200</h1>
       <p>HTML 적용하기</p>
-        <ImportComponent></ImportComponent>
+        <LifecycleEx></LifecycleEx>
     </div>
   );
 }
